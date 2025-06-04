4d0203e0cc0fb16f00347c8bf2f923ef6d3373e5
Leonard Carcaramo <lcarcaramo@ibm.com>
2025-06-04 12:53:16 -0400

    Add hex and binstring examples
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- prototypes/psa.cpp
+++ prototypes/psa.cpp
@@ -1,5 +1,6 @@
-#include <cstring>
+#include <iomanip>
 #include <iostream>
+#include <string>
 
 #include <stdint.h>
 #include <unistd.h>
@@ -23,6 +24,29 @@ int main(void) {
 
   std::cout << "PSA Eyecatcher: " << psapsa << std::endl;
   std::cout << "FLCSVILC: " << static_cast<int>(flcsvilc) << std::endl;
+
+  // FLCRNPSW bitstring and hex portion
+  uint32_t flcrnpsw_bitstring;
+  uint32_t flcrnpsw_hex;
+
+  // Copy first 4 bytes of FLCRNPSW to flcrnpsw_bitstring
+  // Copy the last 4 bytes of FLCRNPSW to flcrnpsw_hex
+  std::memcpy(reinterpret_cast<char *>(&flcrnpsw_bitstring), p_psa->flcrnpsw, 4);
+  std::memcpy(reinterpret_cast<char *>(&flcrnpsw_hex), p_psa->flcrnpsw+4, 4);
+
+  std::cout << "FLCRNPSW (bitstring portion 32-bit): " << std::bitset<32>{flcrnpsw_bitstring} << std::endl;
+  std::cout << "FLCRNPSW (hex portion 4-byte): 0x" << std::hex << std::setfill('0') << std::setw(8) << flcrnpsw_hex << std::endl;
+
+  // FLCCVT address and FLCSOPSW bitstring
+  uint32_t flccvt;
+  uint64_t flcsopsw;
+
+  // Copy FLCVT address and FLCSOPSW bitstring
+  std::memcpy(reinterpret_cast<char *>(&flccvt), p_psa->flccvt, 4);
+  std::memcpy(reinterpret_cast<char *>(&flcsopsw), p_psa->flcsopsw, 8);
+
+  std::cout << "FLCCVT (hex address 4-byte): 0x" << std::hex << std::setfill('0') << std::setw(8) << flccvt << std::endl;
+  std::cout << "FLCSOPSW (bitstirng 64-bit): " << std::bitset<64>{flcsopsw} << std::endl;
 
   return 0;
 }
