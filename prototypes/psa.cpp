c905f0626381cf940ebed0571d16c03ee39f088f
Varun Chennamadhava <varunchennamadhava@ibm.com>
2025-06-26 09:44:18 -0400

    testing

--- prototypes/psa.cpp
+++ prototypes/psa.cpp
@@ -45,5 +45,8 @@ int main(void) {
   std::string flciccw1 = formatter.getString(p_psa->flciccw1, 8);
   std::cout << "FLCICCW1: " << flciccw1 << std::endl;
 
+  std::string flcrnpsw_bitstring = formatter.getBitmap<uint64_t>(p_psa->flcropsw);
+  std::cout << "FLCROPSW: " << flcropsw << std::endl;
+
   return 0;
 }
