60237622756ba34c2fa8d1923ff7cca2aed6fa25
Varun Chennamadhava <varunchennamadhava@ibm.com>
2025-08-19 13:57:11 -0400

    ascb changes

--- cbxp/ascb.cpp
+++ cbxp/ascb.cpp
@@ -11,22 +11,17 @@ namespace ASCB {
 nlohmann::json ASCB::get() {
   // PSA starts at address 0
   struct psa *__ptr32 p_psa = 0;
-  struct cvtmap *__ptr32 p_cvtmap =
-      static_cast<struct cvtmap *__ptr32>(p_psa->flccvt);
+  /*
   struct cvtfix *__ptr32 p_cvtfix =
       static_cast<struct cvtfix *__ptr32>(p_psa->flccvt);
-  struct cvtxtnt2 *__ptr32 p_cvtxtnt2 =
-      static_cast<struct cvtxtnt2 *__ptr32>(p_psa->flccvt);
-  struct cvtvstgx *__ptr32 p_cvtvstgx =
-      static_cast<struct cvtvstgx *__ptr32>(p_psa->flccvt);
-
+  */
   // Get fields
-  nlohmann::json cvt_json = {};
+  nlohmann::json ascb_json = {};
 
-  cvt_json["cvtabend"]    = formatter_.getHex<uint32_t>(p_cvtmap->cvtabend);
+  ascb_json["psaaold"] = formatter_.getHex<uint32_t>(p_psa->psaaold);
  
 
-  return cvt_json;
+  return ascb_json;
 }
 }  // namespace CBXP
 
